5a70f3f38763467de07fe118c7e60d8e9ecf2dfd
He Sichao <1310722434@qq.com>
2024-06-29 12:30:33 +0800

    Update

--- brainunit/math/_fun_accept_unitless_test.py
+++ brainunit/math/_fun_accept_unitless_test.py
@@ -15,6 +15,9 @@ fun_accept_unitless_unary = [
   'arctanh', 'cos', 'cosh', 'sin', 'sinc', 'sinh', 'tan',
   'tanh',
 ]
+fun_accept_unitless_unary_2_results = [
+  'modf', 'frexp',
+]
 fun_accept_unitless_binary = [
   'hypot', 'arctan2', 'logaddexp', 'logaddexp2',
   'corrcoef', 'correlate', 'cov',
@@ -61,6 +64,36 @@ class TestFunAcceptUnitless(parameterized.TestCase):
 
       with pytest.raises(DimensionMismatchError):
         result = bm_fun(q, unit_to_scale=bu.second)
+
+  @parameterized.product(
+    value=[(1.123, 2.567, 3.891), (1.23, 2.34, 3.45)]
+  )
+  def test_fun_accept_unitless_binary_2_results(self, value):
+    bm_fun_list = [getattr(bm, fun) for fun in fun_accept_unitless_unary_2_results]
+    jnp_fun_list = [getattr(jnp, fun) for fun in fun_accept_unitless_unary_2_results]
+
+    for bm_fun, jnp_fun in zip(bm_fun_list, jnp_fun_list):
+      print(f'fun: {bm_fun.__name__}')
+      result1, result2 = bm_fun(jnp.array(value))
+      expected1, expected2 = jnp_fun(jnp.array(value))
+      assert_quantity(result1, expected1)
+      assert_quantity(result2, expected2)
+
+      q = value * meter
+      result1, result2 = bm_fun(q, unit_to_scale=meter)
+      expected1, expected2 = jnp_fun(jnp.array(value))
+      if bm_fun.__name__ == 'modf':
+        assert_quantity(result1, expected1, meter)
+        assert_quantity(result2, expected2, meter)
+      else:
+        assert_quantity(result1, expected1)
+        assert_quantity(result2, expected2)
+
+      with pytest.raises(AssertionError):
+        result1, result2 = bm_fun(q)
+
+      with pytest.raises(DimensionMismatchError):
+        result1, result2 = bm_fun(q, unit_to_scale=bu.second)
 
   @parameterized.product(
     value=[[(1.0, 2.0), (3.0, 4.0), ],
@@ -137,7 +170,6 @@ class TestFunAcceptUnitless(parameterized.TestCase):
       with pytest.raises(AssertionError):
         result = bm_fun(q)
 
-
   @parameterized.product(
     value=[[(0, 1), (1, 1)],
            [(True, False, True, False), (False, False, True, True)]]
@@ -162,47 +194,3 @@ class TestFunAcceptUnitless(parameterized.TestCase):
 
       with pytest.raises(AssertionError):
         result = bm_fun(q1, q2)
-
-  @parameterized.product(
-    value=[(1.123, 2.567, 3.891), (1.23, 2.34, 3.45)]
-  )
-  def test_modf(self, value):
-
-    result1, result2 = bm.modf(jnp.array(value))
-    expected1, expected2 = jnp.modf(jnp.array(value))
-    assert_quantity(result1, expected1)
-    assert_quantity(result2, expected2)
-
-    q = value * meter
-    result1, result2 = bm.modf(q, unit_to_scale=meter)
-    expected1, expected2 = jnp.modf(jnp.array(value))
-    assert_quantity(result1, expected1, meter)
-    assert_quantity(result2, expected2, meter)
-
-    with pytest.raises(AssertionError):
-      result1, result2 = bm.modf(q)
-
-    with pytest.raises(DimensionMismatchError):
-      result1, result2 = bm.modf(q, unit_to_scale=bu.second)
-
-  @parameterized.product(
-    value=[(1.123, 2.567, 3.891), (1.23, 2.34, 3.45)]
-  )
-  def test_frexp(self, value):
-
-    result1, result2 = bm.frexp(jnp.array(value))
-    expected1, expected2 = jnp.frexp(jnp.array(value))
-    assert_quantity(result1, expected1)
-    assert_quantity(result2, expected2)
-
-    q = value * meter
-    result1, result2 = bm.frexp(q, unit_to_scale=meter)
-    expected1, expected2 = jnp.frexp(jnp.array(value))
-    assert_quantity(result1, expected1)
-    assert_quantity(result2, expected2)
-
-    with pytest.raises(AssertionError):
-      result1, result2 = bm.frexp(q)
-
-    with pytest.raises(DimensionMismatchError):
-      result1, result2 = bm.frexp(q, unit_to_scale=bu.second)
--- brainunit/math/_fun_array_creation_test.py
+++ brainunit/math/_fun_array_creation_test.py
@@ -2,14 +2,30 @@ import unittest
 import brainunit as bu
 import brainunit.math as bm
 import inspect
+import jax.numpy as jnp
+
+import pytest
+from absl.testing import parameterized
+
+from brainunit import second, meter, DimensionMismatchError, assert_quantity
 
 fun_array_creation_given_shape = [
-  'full', 'eye', 'identity', 'tri',
   'empty', 'ones', 'zeros',
 ]
+fun_array_creation_given_shape_fill_value = [
+  'full',
+]
+fun_array_creation_given_int = [
+  'eye', 'identity', 'tri',
+]
 fun_array_creation_given_array = [
-  'full_like', 'diag', 'tril', 'triu',
-  'empty_like', 'ones_like', 'zeros_like', 'fill_diagonal',
+  'empty_like', 'ones_like', 'zeros_like', 'diag',
+]
+fun_array_creation_given_array_fill_value = [
+  'full_like',
+]
+fun_array_creation_given_square_array = [
+  'tril', 'triu', 'fill_diagonal',
 ]
 fun_array_creation_misc = [
   'array', 'asarray', 'arange', 'linspace', 'logspace',
@@ -27,14 +43,89 @@ fun_array_creation_other = [
 ]
 
 
-class TestFunArrayCreation(unittest.TestCase):
-  def test_fun_array_creation_given_shape(self):
-    fun_list = [getattr(bm, fun) for fun in fun_array_creation_given_shape]
-    # TODO
+class TestFunArrayCreation(parameterized.TestCase):
 
-  def test_fun_array_creation_given_array(self):
-    fun_list = [getattr(bm, fun) for fun in fun_array_creation_given_array]
-    # TODO
+  @parameterized.product(
+    shape=[(1,), (2, 3), (4, 5, 6)],
+    unit=[second, meter]
+  )
+  def test_fun_array_creation_given_shape(self, shape, unit):
+    bm_fun_list = [getattr(bm, fun) for fun in fun_array_creation_given_shape]
+    jnp_fun_list = [getattr(jnp, fun) for fun in fun_array_creation_given_shape]
+
+    for bm_fun, jnp_fun in zip(bm_fun_list, jnp_fun_list):
+      print(f'fun: {bm_fun.__name__}')
+
+      result = bm_fun(shape)
+      expected = jnp_fun(shape)
+      assert_quantity(result, expected)
+
+      result = bm_fun(shape, unit=unit)
+      expected = jnp_fun(shape)
+      assert_quantity(result, expected, unit=unit)
+
+  @parameterized.product(
+    shape=[(1,), (2, 3), (4, 5, 6)],
+    unit=[second, meter],
+    fill_value=[-1., 1.]
+  )
+  def test_fun_array_creation_given_shape_fill_value(self, shape, unit, fill_value):
+    bm_fun_list = [getattr(bm, fun) for fun in fun_array_creation_given_shape_fill_value]
+    jnp_fun_list = [getattr(jnp, fun) for fun in fun_array_creation_given_shape_fill_value]
+
+    for bm_fun, jnp_fun in zip(bm_fun_list, jnp_fun_list):
+      print(f'fun: {bm_fun.__name__}')
+
+      result = bm_fun(shape, fill_value=fill_value)
+      expected = jnp_fun(shape, fill_value=fill_value)
+      assert_quantity(result, expected)
+
+      result = bm_fun(shape, fill_value=fill_value * unit)
+      expected = jnp_fun(shape, fill_value=fill_value)
+      assert_quantity(result, expected, unit=unit)
+
+
+  @parameterized.product(
+    array=[jnp.array([1.0, 2.0]), jnp.array([[1.0, 2.0], [3.0, 4.0]])],
+    unit=[second, meter]
+  )
+  def test_fun_array_creation_given_array(self, array, unit):
+    bm_fun_list = [getattr(bm, fun) for fun in fun_array_creation_given_array]
+    jnp_fun_list = [getattr(jnp, fun) for fun in fun_array_creation_given_array]
+
+    for bm_fun, jnp_fun in zip(bm_fun_list, jnp_fun_list):
+      print(f'fun: {bm_fun.__name__}')
+
+      result = bm_fun(array)
+      expected = jnp_fun(array)
+      assert_quantity(result, expected)
+
+      result = bm_fun(array, unit=unit)
+      expected = jnp_fun(array)
+      assert_quantity(result, expected, unit=unit)
+
+  @parameterized.product(
+    array=[jnp.array([1.0, 2.0]), jnp.array([[1.0, 2.0], [3.0, 4.0]])],
+    unit=[second, meter],
+    fill_value=[-1., 1.]
+  )
+  def test_fun_array_creation_given_array_fill_value(self, array, unit, fill_value):
+    bm_fun_list = [getattr(bm, fun) for fun in fun_array_creation_given_array_fill_value]
+    jnp_fun_list = [getattr(jnp, fun) for fun in fun_array_creation_given_array_fill_value]
+
+    for bm_fun, jnp_fun in zip(bm_fun_list, jnp_fun_list):
+      print(f'fun: {bm_fun.__name__}')
+
+      result = bm_fun(array, fill_value=fill_value)
+      expected = jnp_fun(array, fill_value=fill_value)
+      assert_quantity(result, expected)
+
+      result = bm_fun(array * unit, fill_value=fill_value * unit)
+      expected = jnp_fun(array, fill_value=fill_value)
+      assert_quantity(result, expected, unit=unit)
+
+      with pytest.raises(AssertionError):
+        result = bm_fun(array, fill_value=fill_value * unit)
 
   def test_fun_array_creation_misc(self):
     fun_list = [getattr(bm, fun) for fun in fun_array_creation_misc]
